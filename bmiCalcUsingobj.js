bac00aafeeb75e1a31e9b43ea906d1ba23b66cce
saranshlakra <saranshlakra4@gmail.com>
2021-01-13 13:49:44 +0530

    Update bmiCalcUsingobj.js

--- bmiCalcUsingobj.js
+++ bmiCalcUsingobj.js
@@ -6,32 +6,32 @@ let lakra = {
         this.BMI = this.weight/(this.height*this.height);
         return this.BMI;
     }
-    // other way
+    //-----other way-----
     // calcBMI: function(a,b){
     //     this.BMI = a/(b*b);
     //     return this.BMI;
-    // }  
+    // }
 }
 
 let gupta = {
     fullName: 'Abhishek Gupta',
     height: 1.78,
     weight: 90,
-    calcBMI: function(){
-        this.BMI = this.weight/(this.height*this.height);
-        return this.BMI;
-    }
+    calcBMI: lakra.calcBMI // calling or borrowing others function.
 }
+lakra.calcBMI();
+gupta.calcBMI();
 
 // console.log(lakra.calcBMI(68,1.75));
-console.log(lakra.calcBMI());
-console.log(gupta.calcBMI());
+// console.log(lakra.calcBMI());
+// console.log(gupta.calcBMI());
 // console.log(lakra);
 // console.log(gupta);
 
 if( lakra.BMI > gupta.BMI){
 console.log(` ${lakra.fullName}'s BMI (${lakra.BMI}) is higher than ${gupta.fullName}'s BMI (${gupta.BMI}). `);
 }
+
 else{
     console.log(`${gupta.fullName}'s BMI (${gupta.BMI}) is higher than ${lakra.fullName}'s BMI (${lakra.BMI}).`);
 }
